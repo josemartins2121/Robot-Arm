23b7eaece479c1f3b8326969428e7ef6d6acb9af
josemartins2121 <jose.martins20001@gmail.com>
2022-09-16 19:45:12 +0100

    pins definidos

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
-#include <Servo.h>
+#include "ServoEasing.hpp"
 
-Servo base; 
-Servo low_arm;
-Servo high_arm;
-Servo actuator;
+ServoEasing base; 
+ServoEasing low_arm;
+ServoEasing high_arm;
+ServoEasing actuator;
 
 
-#define pin_base 3
-#define pin_low_arm 5
+#define pin_base 5
+#define pin_low_arm 3
 #define pin_high_arm 10
 #define pin_actuator 9
 
@@ -19,8 +19,11 @@ Servo actuator;
 #define high_arm_inital_pos 100
 #define actuator_inital_pos 0
 
-#define high_arm_max_pos 95
-#define high_arm_min_pos 40 
+#define high_arm_max_pos 170
+#define high_arm_min_pos 120
+#define low_arm_max_pos 110
+#define low_arm_min_pos 40 
+
 
 int base_pos, low_arm_pos, high_arm_pos, actuator_pos;
 
@@ -35,28 +38,22 @@ long previousStepMillis = 0;
 
 
 void setup() {
-  //setup pins of servo motors 
-  //base.attach(pin_base);
-  //low_arm.attach(pin_low_arm);
-  high_arm.attach(pin_high_arm);
-  //actuator.attach(pin_actuator);
 
-  // setup pos servos 
-  //base.write(base_inital_pos);
-  //low_arm.write(low_arm_inital_pos);
-  //high_arm.write(high_arm_inital_pos);
-  //actuator.write(actuator_inital_pos);
-
-  base_pos = base_inital_pos;
-  low_arm_pos = low_arm_inital_pos;
-  high_arm_pos = high_arm_inital_pos;
-  actuator_pos = actuator_inital_pos;
   Serial.begin(9600);
+
+
+  Serial.println(F("Attach servo at pin " STR(pin_high_arm)));
+  high_arm.attach(pin_high_arm,170);
+  low_arm.attach(pin_low_arm, 40);
+  actuator.attach(pin_actuator, 180);
+  base.attach(pin_base, 180);
+  
+
+  delay(500); // Wait for servo to reach start position.
 }
 
 
 void loop() {
-  int pos;
   // put your main code here, to run repeatedly:
 
  /*  currentMillis = millis();
@@ -82,8 +79,14 @@ void loop() {
   Serial.println(high_arm_pos_smooth);
   high_arm.write(high_arm_pos_smooth); */
 
-
-
+  Serial.println(F("Move to 135 degree with 40 degree per second blocking"));
+  high_arm.startEaseTo(high_arm_max_pos, 30, START_UPDATE_BY_INTERRUPT); 
+  actuator.startEaseTo(180, 20, START_UPDATE_BY_INTERRUPT); 
+  delay(5000);
+  Serial.println(F("Move to 45 degree  with 40 degree per second blocking"));
+  high_arm.startEaseTo(high_arm_min_pos, 30, START_UPDATE_BY_INTERRUPT); 
+  actuator.startEaseTo(0, 60, START_UPDATE_BY_INTERRUPT); 
+  delay(5000);
 
 
 }
